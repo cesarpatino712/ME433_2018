55e5b552da260be80c705abd36228e4baa211db4
cesarpatino712 <35640118+cesarpatino712@users.noreply.github.com>
2018-05-28 22:30:12 -0500

    Update app.c

--- HW8/app.c
+++ HW8/app.c
@@ -55,6 +55,17 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 
 #include "app.h"
 
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include<stdio.h>  
+#include "i2c_master_noint.h"
+#include"ST7735.h"
+//LSsMDS33 SLAVE ADDRESS
+#define I2C_ADDR 0b1101011
+//LSM6DS33 Registers
+#define CTR1_XL 0b10000010                 //linear acceleration sensor 1.66kHz, 2g, 100Hz
+#define CTRL2_G 0b10001000                 //gyroscope 1.66kHz, 1000 dps
+#define CTRL3_C 0b00000100                 //IF_INC bit enabled for sequential read
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data Definitions
@@ -96,7 +107,112 @@ APP_DATA appData;
 
 /* TODO:  Add any necessary local functions.
 */
+void LCD_init(void);
+void I2C_read_multiple(unsigned char SLAVEaddr, unsigned char addr, unsigned char * data, int length);
+unsigned char getIMU(unsigned char SLAVEaddr, unsigned char addr);
+void setIMU(unsigned char SLAVEaddr, unsigned char addr, unsigned char DIN);
+void initIMU(void);
+void drawChar(char x, char y, char message, short colorON, short colorOFF);
+void drawString(char x, char y, char* message, short colorOn, short colorOFF);
+void drawProgressBar(char x, char y,int length, short colorON, short colorOFF);
 
+void I2C_read_multiple(unsigned char SLAVEaddr, unsigned char addr, unsigned char * data, int length){
+    //i2C read sequence S -> OP -> W -> ADDR -> SR -> OP -> R -> DOUT -> P
+    // S = start; OP = device opcode; W =  write; ADDR = register adress -> SR = restart; R = read; DOUT =data out; P = stop
+    //device opcode: 0 1 0 0 A2 A1 A0 R/W; A2 A1 A0 = address, R/W read = 1  write = 0;
+   
+    int i = 0;
+    i2c_master_start();                                     //start
+    i2c_master_send(SLAVEaddr<<1);            //write 
+    i2c_master_send(addr);                  //register address 
+    i2c_master_restart();                                //restart
+    i2c_master_send((SLAVEaddr<<1)|1);            //read
+    for (i = 0; i < length; i++){
+        data[i] = i2c_master_recv();
+        if (i < length - 1){
+            i2c_master_ack(0);              //keep reading
+        }
+        else{
+            i2c_master_ack(1);
+        }
+    }
+
+    i2c_master_stop();
+    
+}
+
+
+unsigned char getIMU(unsigned char SLAVEaddr, unsigned char addr){
+    //i2C read sequence S -> OP -> W -> ADDR -> SR -> OP -> R -> DOUT -> P
+    // S = start; OP = device opcode; W =  write; ADDR = register adress -> SR = restart; R = read; DOUT =data out; P = stop
+    //device opcode: 0 1 0 0 A2 A1 A0 R/W; A2 A1 A0 = address, R/W read = 1  write = 0;
+    i2c_master_start();                                     //start
+    i2c_master_send(SLAVEaddr<<1);            //write 
+    i2c_master_send(addr);                  //register address 
+    i2c_master_restart();                                //restart
+    i2c_master_send((SLAVEaddr<<1)|1);            //read
+    unsigned char DOUT = i2c_master_recv();                              //receive data
+    i2c_master_ack(1);
+    i2c_master_stop();
+    return DOUT;
+    
+}
+
+void setIMU(unsigned char SLAVEaddr, unsigned char addr, unsigned char DIN){
+    i2c_master_start();
+    i2c_master_send(SLAVEaddr<<1);           //write 
+    i2c_master_send(addr);                 //register address                              
+    i2c_master_send(DIN);            //write
+    i2c_master_stop();
+}
+  void initIMU(void){
+    //clear analog input pins
+    ANSELBbits.ANSB2 = 0;           //SCL
+    ANSELBbits.ANSB3 = 0;           //SDA
+    i2c_master_setup();             //BRG = 53 for 400KHZ
+    //set registers
+    setIMU(I2C_ADDR, 0x10, CTR1_XL);
+    setIMU(I2C_ADDR, 0x11, CTRL2_G);
+    setIMU(I2C_ADDR, 0x12, CTRL3_C);
+       
+}
+
+void drawChar(char x, char y, char message, short colorON, short colorOFF){
+    int col = 0;
+    int j = 0;
+    char pixels;
+    
+    for(col = 0; col < 5; col++){
+        pixels = ASCII[message - 0x20][col];
+        for(j = 0; j < 8; j++){
+            if ((pixels >> j) & 1){
+                LCD_drawPixel(x + col, y + j, colorON);
+            }
+            else {
+                LCD_drawPixel(x + col, y + j, colorOFF);
+            }
+        }
+    }
+}
+
+void drawString(char x, char y, char* message, short colorOn, short colorOFF){
+    int i = 0;
+    while(message[i]){
+        drawChar(x + 5 * i, y, message[i], colorOn, colorOFF);
+        i++;
+    }
+}
+
+void drawProgressBar(char x, char y,int length, short colorON, short colorOFF){
+    int i = 0;
+    int j = 0;
+    for(i = 0; i < length; i++){
+        LCD_drawPixel(x + i, y, colorON);
+        for (j = x+ i; j < x + length; j++){
+        LCD_drawPixel(j - i, y, colorOFF);
+        }
+    }
+}
 
 // *****************************************************************************
 // *****************************************************************************
@@ -116,10 +232,15 @@ void APP_Initialize ( void )
 {
     /* Place the App state machine in its initial state. */
     appData.state = APP_STATE_INIT;
-// do your TRIS and LAT commands here
+    
+    // do your TRIS and LAT commands here
     TRISBbits.TRISB4=1;
     TRISAbits.TRISA4=0;
     LATAbits.LATA4=1;
+    
+    initIMU();
+    LCD_init();
+
     
     /* TODO: Initialize your application's state machine and other
      * parameters.
@@ -157,19 +278,59 @@ void APP_Tasks ( void )
 
         case APP_STATE_SERVICE_TASKS:
         {
-            _CP0_SET_COUNT(0);
-            while(1) {
+             char message[30];
+    //sprintf(message,"Hello World");
+    LCD_clearScreen(BLACK);
+    unsigned char data [13];
+    signed short scale = 16500;
+
+    while(1) {
+        //if (PORTBbits.RB4 == 1){        //push button is not pressed
 	// use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 	// remember the core timer runs at half the sysclk
-        while (PORTBbits.RB4 == 0){
-            LATAbits.LATA4 = 0;
+        //start the timer at 0
+        _CP0_SET_COUNT(0);
+        LATAINV = 0x10;                  //toggle LATA4 LED on
+       
+         
+        I2C_read_multiple(I2C_ADDR, 0x20, data, 14);
+        
+        //reconstruct signed short by shifting high byte and ORing with the low byte
+        //angular rate sensor pitch axis (x)
+        //Gyroscope readings
+        signed short OUTX_G = ((data[3] << 8)| data[2]);
+        sprintf(message,"OUTX_G = %d",OUTX_G);
+        drawString(28, 12, message, RED, BLACK);
+        signed short OUTY_G = ((data[5] << 8)| data[4]);
+        sprintf(message,"OUTY_G = %d",OUTY_G);
+        drawString(28, 22, message, RED, BLACK);
+        signed short OUTZ_G = ((data[7] << 8)| data[6]);
+        sprintf(message,"OUTZ_G = %d",OUTZ_G);
+        drawString(28, 32, message, RED, BLACK);
+        //Accelerometer Readings
+        signed short OUTX_XL = ((data[9] << 8)| data[8]);
+        sprintf(message,"OUTX_XL = %d",OUTX_XL);
+        drawString(28, 42, message, CYAN, BLACK);
+        signed short OUTY_XL = ((data[11] << 8)| data[10]);
+        sprintf(message,"OUTY_XL = %d",OUTY_XL);
+        drawString(28, 52, message, CYAN, BLACK);
+        signed short OUTZ_XL = ((data[13] << 8)| data[12]);
+        sprintf(message,"OUTZ_XL = %d",OUTZ_XL);
+        drawString(28, 62, message, CYAN, BLACK);
+        
+        
+        
+        // 20 Hz delay
+        while(_CP0_GET_COUNT()<24000000/20){
+           
         }
         
-        if (_CP0_GET_COUNT()>1200000){
-            LATAbits.LATA4 = ! LATAbits.LATA4;
-            _CP0_SET_COUNT(0);
-        }
-            }
+        
+        
+    }
+    return 0;
+          
+        
             break;
         }
 
